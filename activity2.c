561006176493c4f01484980ce7459de1822e9f72
99003660 <78850394+99003660@users.noreply.github.com>
2021-03-09 08:24:08 +0530

    Add files via upload

--- activity2.c
+++ activity2.c
@@ -1,21 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include<stdio.h>
+
+struct colour_box{
+    int id;
+    float len;
+    float bre;
+    float h;
+    char colour[10];
+    float weight;
+};
 
 int main()
 {
-	int ret = fork();
-	if(ret == 0)
-	{
-		execlp("./b.out",NULL);
-		exit(1);
-	}
-	else
-	{
-		printf("parent\n");
-		sleep(1);
-	}
-	return 0;
+    int *arr;
+    int i, num;
+    struct colour_box box;
+    printf("enter the num of boxes:");
+    scanf("%d",&num);
+
+    box=(int*)malloc(sizeof(int)*num);
+
+    for(i=0;i<num;i++)
+    {
+        scanf("%d\n",&box[i].id);
+        scanf("%f\n",&box[i].len);
+        scanf("%f\n",&box[i].bre);
+        scanf("%f\n",&box[i].h);
+        scanf("%s\n",&box[i].colour);
+        scanf("%f\n",&box[i].weight);
+    }
+
+    printf("enter the id to search:");
+    
+
+
 }
